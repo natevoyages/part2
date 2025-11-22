52d3c5be94f70685f343a4c85100c0a546ad68b0
NateVoyages <allanromo20@gmail.com>
2025-11-22 15:20:49 -0800

    Add number functionality + Complete assignment 2.8

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 
-const People = ({persons}) =>  <p>{persons.name}</p>
+const People = ({persons}) =>  <p>{persons.name} {persons.number}</p>
 
 
 const App = () => {
@@ -10,6 +10,7 @@ const App = () => {
      }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
   const addName = (event) =>  { 
     event.preventDefault()
     const names = persons.keys()
@@ -22,13 +23,17 @@ const App = () => {
     console.log(newName)
     const updatedList = persons.concat({
       name: newName,
+      number: newNumber,
       id: persons.length + 1,
     })
     setPersons(updatedList)
 
   }
-  const handlePersonChange = (event) => {
+  const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -36,8 +41,9 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={addName}>
         <div>
-          name: <input value={newName} onChange={handlePersonChange}/>
-          <div>debug: {newName}</div>
+          <div>name: <input value={newName} onChange={handleNameChange}/></div>
+          <div>number: <input value={newNumber} onChange={handleNumberChange} /></div>
+          <div>debug: {newName} {newNumber}</div>
         </div>
         <div>
           <button type="submit">add</button>
