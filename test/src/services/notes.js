9a138d0d9704b758650017108a741ab0036f8c70
NateVoyages <allanromo20@gmail.com>
2026-01-03 18:46:52 -0800

    Add clean syntax literal

--- test/src/services/notes.js
+++ test/src/services/notes.js
@@ -1,9 +1,14 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/person'
+const baseUrl = 'http://localhost:3001/notes'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
-  return request.then(response => response.data)
+  const nonExisting = {
+    id: 10000,
+    content: 'This note is not saved to server',
+    important: true,
+  }
+  return request.then(response => response.data.concat(nonExisting))
 }
 
 const create = newObject => {
@@ -15,5 +20,6 @@ const update = (id, newObject) => {
   const request = axios.put(`${baseUrl}/${id}`, newObject)
   return request.then(response => response.data)
 }
+
 
 export default { getAll, create, update }
